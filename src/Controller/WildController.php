07681eb566df69493e22fae49d300e0b403d128d
Krilline <krillinee@gmail.com>
2020-06-09 12:18:16 +0200

    completed symfony20

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -139,6 +139,23 @@ class WildController extends AbstractController
     }
 
     /**
+     * @Route("/{id}", name="comment_delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Comment $comment
+     * @return Response
+     */
+    public function delete(Request $request, Comment $comment): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$comment->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($comment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('wild_index');
+    }
+
+    /**
      * @Route("/actor/{slug}", name="show_actor")
      * @param Actor $actor
      * @return Response
